a298ea6cdaef1380bb9cd857f2278c30d1035bb7
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-03-14 16:13:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import entradaDados from 'readline-sync';
+
+console.log("Escolha uma das alternativas:");
+console.log("1 - Listar os salários mínimos de 2010 à 2020");
+console.log("2 - Listar o IPCA de 2010 à 2020");
+console.log("3 - Comparação entre o percentual de aumento salarial e o IPCA");
+
+console.log("Digite o número da sua escolha: ")
+
+let salarioMinimo = [
+    {ano: 2010, salario: 510.00},
+    {ano: 2011, salario: 510.00},
+    {ano: 2012, salario: 510.00},
+    {ano: 2013, salario: 510.00},
+    {ano: 2014, salario: 510.00},
+    {ano: 2015, salario: 510.00},
+    {ano: 2016, salario: 510.00},
+    {ano: 2017, salario: 510.00},
+    {ano: 2018, salario: 510.00},
+    {ano: 2019, salario: 510.00},
+    {ano: 2020, salario: 510.00}
+];
+
+let salarioMinimo = [
+    {ano: 2010, ipca: 510.00},
+    {ano: 2011, ipca: 510.00},
+    {ano: 2012, ipca: 510.00},
+    {ano: 2013, ipca: 510.00},
+    {ano: 2014, ipca: 510.00},
+    {ano: 2015, ipca: 510.00},
+    {ano: 2016, ipca: 510.00},
+    {ano: 2017, ipca: 510.00},
+    {ano: 2018, ipca: 510.00},
+    {ano: 2019, ipca: 510.00},
+    {ano: 2020, ipca: 510.00}
+];

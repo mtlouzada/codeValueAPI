c067bca07fe85a249eaa0d5aa29ae1d8680213c1
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-05-13 23:12:19 -0300

    new skelleton fom variebles and better arrays syntax

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-import entradaDados from 'readline-sync';
+import entradaDados from 'readline-sync'
 
-let opcao1  = 1;
-let opcao2 = 2;
-let opcao3 = 3;
-
-var salarioMinimo = [
+let salarioMinimo = [
     {ano: 2010, salario: 510.00},
-    {ano: 2011, salario: 510.00},
-    {ano: 2012, salario: 510.00},
-    {ano: 2013, salario: 510.00},
-    {ano: 2014, salario: 510.00},
-    {ano: 2015, salario: 510.00},
-    {ano: 2016, salario: 510.00},
-    {ano: 2017, salario: 510.00},
-    {ano: 2018, salario: 510.00},
-    {ano: 2019, salario: 510.00},
-    {ano: 2020, salario: 510.00}
+    {ano: 2011, salario: 545.00},
+    {ano: 2012, salario: 622.00},
+    {ano: 2013, salario: 678.00},
+    {ano: 2014, salario: 724.00},
+    {ano: 2015, salario: 788.00},
+    {ano: 2016, salario: 880.00},
+    {ano: 2017, salario: 937.00},
+    {ano: 2018, salario: 954.00},
+    {ano: 2019, salario: 998.00},
+    {ano: 2020, salario: 1045.00}
 ];
 
-var precoInflacao = [
-    {ano: 2010, ipca: 510.00},
-    {ano: 2011, ipca: 510.00},
-    {ano: 2012, ipca: 510.00},
-    {ano: 2013, ipca: 510.00},
-    {ano: 2014, ipca: 510.00},
-    {ano: 2015, ipca: 510.00},
-    {ano: 2016, ipca: 510.00},
-    {ano: 2017, ipca: 510.00},
-    {ano: 2018, ipca: 510.00},
-    {ano: 2019, ipca: 510.00},
-    {ano: 2020, ipca: 510.00}
+let Inflacao = [
+    {ano: 2010, ipca: 5.91},
+    {ano: 2011, ipca: 6.50},
+    {ano: 2012, ipca: 5.84},
+    {ano: 2013, ipca: 5.91},
+    {ano: 2014, ipca: 6.41},
+    {ano: 2015, ipca: 10.67},
+    {ano: 2016, ipca: 6.29},
+    {ano: 2017, ipca: 2.95},
+    {ano: 2018, ipca: 3.75},
+    {ano: 2019, ipca: 4.31},
+    {ano: 2020, ipca: 4.52}
 ];
 
-console.log("\nEscolha uma das alternativas: \n");
+console.log("\nEscolha uma das alternativas: \n")
+
 console.log("1 - Listar os salários mínimos de 2010 à 2020");
 console.log("2 - Listar o IPCA de 2010 à 2020");
 console.log("3 - Comparação entre o percentual de aumento salarial e o IPCA");
 
-console.log("\nDigite o número da sua escolha: \n")
+let escolha = entradaDados.question("\nDigite o número da sua escolha: \n")
+
+if(Number(escolha) == 1){
+    console.log(salarioMinimo.length)
+}
+else if{
+
+}
+
+else{
+    
+}
+
+
+
+
+
 
